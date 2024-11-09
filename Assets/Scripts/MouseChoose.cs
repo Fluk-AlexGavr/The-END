1db36ebb9b8d72101ff56c2c997980c1650a7253
Fluk <aleshagavr@mail.ru>
2024-11-10 01:26:15 +0300

    add collect items feature

--- Assets/Scripts/MouseChoose.cs
+++ Assets/Scripts/MouseChoose.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MouseChoose : MonoBehaviour
 {
     [SerializeField] Light light;
+    [SerializeField] Material material;
     [SerializeField] float intensityNotChoosen;
     [SerializeField] float intensityChoosen;
+    Transform Player;
+    [SerializeField] float distanceGrab; 
+    ChangeWeapon ChangeWeaponScript;
+    [SerializeField] Color ImageColorActive;
+    [SerializeField] Color ImageColorNotActive;
+    [SerializeField] Image image;
+
+    private void Start()
+    {
+        Player = GameObject.FindWithTag("Player").transform;
+        ChangeWeaponScript = Player.GetComponent<ChangeWeapon>();
+    }
+
     private void OnMouseEnter()
     {
+        material.SetFloat("_Outline_Range", 1.05f);
         light.intensity = intensityChoosen;
+        if ((Player.position - transform.position).magnitude < distanceGrab)
+        {
+            image.color = ImageColorActive;
+        }
     }
     private void OnMouseExit()
     {
+        material.SetFloat("_Outline_Range", 0.05f);
         light.intensity = intensityNotChoosen;
+        image.color = ImageColorNotActive;
+    }
+
+    private void OnMouseDown()
+    {
+        if ((Player.position - transform.position).magnitude < distanceGrab)
+        {
+            Destroy(gameObject);
+            material.SetFloat("_Outline_Range", 1.05f);
+            image.color = ImageColorNotActive;
+            SetWeapon();
+        }
+    }
+
+    private void SetWeapon()
+    {
+        print(gameObject.tag);
+        ChangeWeaponScript.weaponName = gameObject.tag;
     }
 }
